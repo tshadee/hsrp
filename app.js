ea9bf99b919baab672e305db08f76e8640732c6f
Pannawich Techadee <pan.techadee@gmail.com>
2025-05-15 01:12:48 +0100

    animations and buttons. need to work on js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks= document.querySelector('.navbar__menu')
+const hiddenLink = document.querySelector('.hidden-link')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
+})
+
+hiddenLink.addEventListener('mouseover', function() {
+    hiddenLink.classList.add(visible);
+})
+
+hiddenLink.addEventListener('mouseout', ()=> {
+    setTimeout(() => {
+        hiddenLink.classList.remove(visible);
+    }, 500);
 })
